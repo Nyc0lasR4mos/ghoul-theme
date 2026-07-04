1e39254af73451d8c14a29756c729b13487d9667
Nycolas Ramos <nyckgabramos@gmail.com>
2026-07-04 01:43:42 -0300

    Initial commit: Ghoul Theme - Tokyo Ghoul inspired terminal theme

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,5 @@
+oh-my-posh init pwsh --config 'C:\Users\Nycoteba\PowerShell\GhoulTheme.omp.json' | Invoke-Expression
+
+fastfetch --config "$env:USERPROFILE\.config\fastfetch\config-mini.jsonc"
+
+$Host.UI.RawUI.WindowTitle = "PowerShell"
